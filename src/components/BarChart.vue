5ad8feba53f5ac705c882565503db990ad4751db
零距离 <98647744+asxas54@users.noreply.github.com>
2023-02-18 15:23:31 +0800

    Add files via upload

---
+++ src/components/BarChart.vue
@@ -0,0 +1,11 @@
+<template>
+    <h>条形图</h>
+</template>
+
+<script></script>
+
+<style>
+    h{
+        color:white;
+    }
+</style>
---
+++ src/components/ScatterChart.vue
@@ -0,0 +1,11 @@
+<template>
+    <h>散点图</h>
+</template>
+
+<script></script>
+
+<style>
+h{
+    color:white;
+}
+</style>
